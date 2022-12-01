176565e8dbe43c092ea4b63061d2c7c124df63a0
eugene <bantyc8@gmail.com>
2022-12-01 17:53:45 +0200

    Add ordering by name & information display by __str__ method in TaskType model

--- task_manager/models.py
+++ task_manager/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class TaskType(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
