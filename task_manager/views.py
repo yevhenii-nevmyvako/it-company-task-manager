0313f53fa22b507ddeb0e188fe2d2655b39bad3d
eugene <bantyc8@gmail.com>
2022-12-02 17:37:31 +0200

    Add includes from local task_manager/urls.py to global & run empty index.html page

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse(
+        "<html>"
+        "<h1>task manager</h1>"
+        "</html>"
+
+    )
