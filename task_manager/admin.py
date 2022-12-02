071d3e632dbb60ba878f16aa16dc2c216b1e7eb6
eugene <bantyc8@gmail.com>
2022-12-02 16:44:06 +0200

    Add search fields & list_filter to all field on admin page

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -4,24 +4,25 @@ from django.contrib.auth.admin import UserAdmin
 from task_manager.models import TaskType, Task, Position, Worker
 
 
+@admin.register(TaskType)
 class TaskTypeAdmin(admin.ModelAdmin):
     list_display = ["name"]
+    list_filter = ["name"]
+    search_fields = ["name"]
 
 
-admin.site.register(TaskType, TaskTypeAdmin)
-
-
+@admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ["is_completed", "priority", "task_type", "deadline"]
+    list_display = ["is_completed", "priority", "task_type", "deadline", "description"]
+    list_filter = ["is_completed", "priority", "deadline"]
+    search_fields = ["task_type"]
 
 
-admin.site.register(Task, TaskAdmin)
-
-
+@admin.register(Position)
 class PositionAdmin(admin.ModelAdmin):
     list_display = ["name"]
+    list_filter = ["name"]
+    search_fields = ["name"]
 
-
-admin.site.register(Position, PositionAdmin)
 
 admin.site.register(Worker, UserAdmin)
