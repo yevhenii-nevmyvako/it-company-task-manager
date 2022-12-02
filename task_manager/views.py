4e8eef7ac3b490f4188dddac5d3c5e6982607a7f
eugene <bantyc8@gmail.com>
2022-12-02 20:10:54 +0200

    Add base.html as main temlate to app task_manager

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,11 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from task_manager.models import TaskType, Task, Position, Worker
+
 
 def index(request):
-    return HttpResponse(
-        "<html>"
-        "<h1>task manager</h1>"
-        "</html>"
+    num_tusk_types = TaskType.objects.count()
+    num_tusks = Task.objects.count()
+    num_positions = Position.objects.count()
+    num_workers = Worker.objects.count()
 
-    )
+    context = {
+        "num_tusk_types": num_tusk_types,
+        "num_tusks": num_tusks,
+        "num_positions": num_positions,
+        "num_workers": num_workers
+    }
+
+    return render(request, "task_manager/index.html", context=context)
