eeecd88eab24b99972afe4885819e017761f0b40
eugene <bantyc8@gmail.com>
2022-12-04 21:22:25 +0200

    Add session counter to index.html

--- task_manager/views.py
+++ task_manager/views.py
@@ -10,12 +10,15 @@ def index(request):
     num_tusks = Task.objects.count()
     num_positions = Position.objects.count()
     num_workers = Worker.objects.count()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
 
     context = {
         "num_tusk_types": num_tusk_types,
         "num_tusks": num_tusks,
         "num_positions": num_positions,
-        "num_workers": num_workers
+        "num_workers": num_workers,
+        "num_visits": num_visits + 1
     }
 
     return render(request, "task_manager/index.html", context=context)
