15973378b985a1df57ad12342f508ca461faf4f3
eugene <bantyc8@gmail.com>
2022-12-05 17:50:06 +0200

    Add empty login.html & logged_out.html to registration direct & add path accounts/ to global urls.py

--- it_company_task_manager/urls.py
+++ it_company_task_manager/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("task_manager.urls", namespace="task_manager")),
     path('__debug__/', include('debug_toolbar.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
