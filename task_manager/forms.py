046cbd48ce4228c3676e412f54a8693bdb219755
eugene <bantyc8@gmail.com>
2022-12-06 17:58:06 +0200

    Add TaskForm to forms.py

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,7 +1,7 @@
-# from django import forms
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
-from task_manager.models import Worker
+from task_manager.models import Worker, Task
 
 
 class WorkerCreationFrom(UserCreationForm):
@@ -9,3 +9,11 @@ class WorkerCreationFrom(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Worker
         fields = UserCreationForm.Meta.fields + ("first_name", "last_name", "position",)
+
+
+class TaskForm(forms.ModelForm):
+
+    class Meta:
+        model = Task
+        fields = "__all__"
+
