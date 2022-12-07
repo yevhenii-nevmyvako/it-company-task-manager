0f5980f627532881dcf7ae01b0670eed3a99cdd9
eugene <bantyc8@gmail.com>
2022-12-07 18:13:17 +0200

    Add TaskSearchForm & widget to forms.py

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -92,3 +92,16 @@ class TaskSearchForm(forms.Form):
             }
         )
     )
+
+
+class TaskTypeSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by name"
+            }
+        )
+    )
