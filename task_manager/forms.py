8409b861fd5885cf8b01866e147034d87c9bd7b2
eugene <bantyc8@gmail.com>
2022-12-07 12:04:38 +0200

    Add datetime widget to deadline field in forms.py

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
 from task_manager.models import Worker, Task
@@ -12,6 +13,18 @@ class WorkerCreationFrom(UserCreationForm):
 
 
 class TaskForm(forms.ModelForm):
+    deadline = forms.DateField(
+        label="Deadline datetime",
+        widget=forms.DateInput(
+            format='%d-%m-%Y', attrs={'type': 'date'}
+        )
+        , required=False
+    )
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
 
     class Meta:
         model = Task
