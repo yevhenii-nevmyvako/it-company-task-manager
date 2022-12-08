abcdd441039f3c5cb10573c43869790f354916f3
eugene <bantyc8@gmail.com>
2022-12-08 11:54:15 +0200

    Change values ForeignKey in Task model to project

--- task_manager/models.py
+++ task_manager/models.py
@@ -71,8 +71,8 @@ class Task(models.Model):
         settings.AUTH_USER_MODEL,
         related_name="tasks"
     )
-    team = models.ForeignKey(
-        Team, on_delete=models.CASCADE, related_name="tasks"
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, related_name="tasks"
     )
 
     class Meta:
