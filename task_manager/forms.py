fd3931a2522e99e423855ad8e0fce803dfa14afd
eugene <bantyc8@gmail.com>
2022-12-22 02:42:22 +0200

    Add validation to TeamForm fields = name & taskForm fields = deadline & deadlineForm dields = deadline

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,7 +1,10 @@
+import datetime
+
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 from task_manager.models import Worker, Task, Position, Team, Project
 
@@ -14,6 +17,13 @@ class WorkerCreationFrom(UserCreationForm):
             "first_name", "last_name", "email", "position"
         )
 
+    def clean_position(self):
+        name = self.cleaned_data["name"]
+
+        if [el for el in name if el in "0123456789"]:
+            raise ValidationError("Should be no digits in position field")
+        return name
+
 
 class TeamForm(forms.ModelForm):
     members = forms.ModelMultipleChoiceField(
@@ -25,6 +35,14 @@ class TeamForm(forms.ModelForm):
     class Meta:
         model = Team
         fields = "__all__"
+
+    def clean_name(self):
+        name = self.cleaned_data["name"]
+
+        if [el for el in name if el in "0123456789"]:
+            raise ValidationError("Should be no digits in this field")
+
+        return name
 
 
 class PositionForm(forms.ModelForm):
@@ -59,6 +77,15 @@ class TaskForm(forms.ModelForm):
         model = Task
         fields = "__all__"
 
+    def clean_deadline(self):
+        deadline = self.cleaned_data["deadline"]
+        deadline = str(deadline)
+        now = str(timezone.now())
+
+        if deadline < now:
+            raise ValidationError("It's date should be in future")
+        return deadline
+
 
 class CompletedForm(forms.ModelForm):
 
@@ -79,6 +106,15 @@ class DeadlineForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ("deadline",)
+
+    def clean_deadline(self):
+        deadline = self.cleaned_data["deadline"]
+        deadline = str(deadline)
+        now = str(timezone.now())
+
+        if deadline < now:
+            raise ValidationError("It's date should be in future")
+        return deadline
 
 
 class PriorityForm(forms.ModelForm):
