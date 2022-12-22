5e44d9479ec46e68eb804c2c37319510f100c192
eugene <bantyc8@gmail.com>
2022-12-22 23:38:30 +0200

    Add test_retrive_worker_detail_with_login & test_retrieve_worker_delete_with_login & test_retrieve_worker_create_with_login & test_retrieve_worker_list_with_with_login

--- task_manager/tests/test_worker_views.py
+++ task_manager/tests/test_worker_views.py
@@ -2,10 +2,13 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 from django.urls import reverse
 
-from task_manager.models import Worker
+from task_manager.models import Worker, Position
 
 WORKER_URL = reverse("task_manager:worker-list")
 WORKER_CREATE_URL = reverse("task_manager:worker-create")
+# WORKER_DETAIL_URL = reverse("task_manager:worker-detail", args=[.id])
+# WORKER_DELETE_URL = reverse("task_manager:worker-delete")
+
 
 
 class PublicWorkerTests(TestCase):
@@ -24,11 +27,44 @@ class PublicWorkerTests(TestCase):
 
 
 class PrivetWorkerTests(TestCase):
+
     def setUp(self) -> None:
         self.user = get_user_model().objects.create_user(
             username="test",
             password="qwer1234"
         )
         self.client.force_login(self.user)
+
+    def test_retrieve_worker_list_with_login(self):
+        """test retrieved worker list with login required"""
+        response = self.client.get(WORKER_URL)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_retrieve_worker_create_with_login(self):
+        """test retrieved worker create page with login required"""
+        response = self.client.get(WORKER_CREATE_URL)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_retrieve_worker_detail_with_login(self):
+        """test retrieved worker detail page with login required"""
+        worker_detail = Worker.objects.create(first_name="test", last_name="testing")
+        url_to_detail = reverse("task_manager:worker-detail", args=[worker_detail.id])
+        response = self.client.get(url_to_detail)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_retrieve_worker_delete_with_login(self):
+        """test retrieved worker delete page with login required"""
+        worker_detail = Worker.objects.create(first_name="test", last_name="testing")
+        url_to_delete = reverse("task_manager:worker-delete", args=[worker_detail.id])
+        response = self.client.get(url_to_delete)
+
+        self.assertEqual(response.status_code, 200)
+
+
+
+
 
 
