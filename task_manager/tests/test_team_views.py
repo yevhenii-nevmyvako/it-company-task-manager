0d25c6f306564cf0dd88e0236483e0e651aa2e98
eugene <bantyc8@gmail.com>
2022-12-23 18:48:44 +0200

    Add test_delete_team_login_required & test_create_team_login_required & test_team_open_list_required to PublicTeamTests in test_team_views.py

--- task_manager/tests/test_team_views.py
+++ task_manager/tests/test_team_views.py
@@ -2,9 +2,36 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from task_manager.models import Project, Team
+from task_manager.models import Team
 
 TEAM_URL = reverse("task_manager:team-list")
 TEAM_CREATE_URL = reverse("task_manager:team-create")
 
 
+class PublicTeamTests(TestCase):
+
+    def test_team_open_list_required(self):
+        """test login required in team
+        list view doesn't work without login"""
+        response = self.client.get(TEAM_URL)
+
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_create_team_login_required(self):
+        """test client doesn't open create
+        team without login required"""
+        response = self.client.get(TEAM_CREATE_URL)
+
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_delete_team_login_required(self):
+        """test doesn't open team_delete_confirm without login"""
+        team = Team.objects.create(
+            name="test"
+        )
+        url_to_delete = reverse(
+            "task_manager:team-delete", args=[team.id]
+        )
+        response = self.client.get(url_to_delete)
+
+        self.assertNotEqual(response.status_code, 200)
