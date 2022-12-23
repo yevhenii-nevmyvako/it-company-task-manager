e75a4f2ccb0f56ddbb718eeb2153e648b87106ac
eugene <bantyc8@gmail.com>
2022-12-23 19:42:05 +0200

    Add test_update_team_login_required & test_detail_task_login_required & test_delete_task_login_required to PublicTaskTests in test_task_views.py

--- task_manager/tests/test_task_views.py
+++ task_manager/tests/test_task_views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from task_manager.models import Project, Team
+from task_manager.models import Project, Team, Task, TaskType
 
 TASK_URL = reverse("task_manager:task-list")
 TASK_CREATE_URL = reverse("task_manager:task-create")
@@ -21,5 +21,44 @@ class PublicTaskTests(TestCase):
         """test client doesn't open create
         task without login required"""
         response = self.client.get(TASK_CREATE_URL)
+
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_delete_task_login_required(self):
+        """test doesn't open task_delete_confirm without login"""
+        task_type = TaskType.objects.create(name="test1")
+        task = Task.objects.create(
+            task_type=task_type
+        )
+        url_to_delete = reverse(
+            "task_manager:task-delete", args=[task.id]
+        )
+        response = self.client.get(url_to_delete)
+
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_detail_task_login_required(self):
+        """test doesn't open task_detail without login"""
+        task_type = TaskType.objects.create(name="test1")
+        task = Task.objects.create(
+            task_type=task_type
+        )
+        url_to_detail = reverse(
+            "task_manager:team-detail", args=[task.id]
+        )
+        response = self.client.get(url_to_detail)
+
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_update_team_login_required(self):
+        """test doesn't open team_form without login"""
+        task_type = TaskType.objects.create(name="test1")
+        task = Task.objects.create(
+            task_type=task_type
+        )
+        url_to_update = reverse(
+            "task_manager:team-update", args=[task.id]
+        )
+        response = self.client.get(url_to_update)
 
         self.assertNotEqual(response.status_code, 200)
