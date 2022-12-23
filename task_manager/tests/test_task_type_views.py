83e0f15f07fb54f8055d261dc41e60eff66407b8
eugene <bantyc8@gmail.com>
2022-12-23 17:37:44 +0200

    Add def test_retrieve_task_type_detail_with_login & def test_retrieve_task_type_delete_with_login & def test_retrieve_task_type_update_with_login to test_task_type_views.py

--- task_manager/tests/test_task_type_views.py
+++ task_manager/tests/test_task_type_views.py
@@ -83,12 +83,49 @@ class PrivateTaskTypeTests(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
-    def test_create_position_with_login_required(self):
-        """test should create position with user login"""
+    def test_create_task_type_with_login_required(self):
+        """test should create task type with user login"""
         TaskType.objects.create(name="test")
         response = self.client.get(TASK_TYPE_CREATE_URL)
 
         self.assertEqual(response.status_code, 200)
+
+    def test_retrieve_task_type_detail_with_login(self):
+        """test retrieved task type detail page with login required"""
+        task_type = TaskType.objects.create(
+            name="test"
+        )
+        url_to_detail = reverse(
+            "task_manager:task-type-detail", args=[task_type.id]
+        )
+        response = self.client.get(url_to_detail)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_retrieve_task_type_delete_with_login(self):
+        """test retrieved position delete page with login required"""
+        task_type = TaskType.objects.create(
+            name="test"
+        )
+        url_to_delete = reverse(
+            "task_manager:task-type-delete", args=[task_type.id]
+        )
+        response = self.client.get(url_to_delete)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_retrieve_task_type_update_with_login(self):
+        """test retrieved task type delete page with login required"""
+        task_type = TaskType.objects.create(
+            name="test"
+        )
+        url_to_update = reverse(
+            "task_manager:task-type-update", args=[task_type.id]
+        )
+        response = self.client.get(url_to_update)
+
+        self.assertEqual(response.status_code, 200)
+
 
 
 
