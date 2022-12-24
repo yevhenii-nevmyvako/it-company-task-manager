5ca8783ca1a7a4b06439f95356eac4a66053080e
eugene <bantyc8@gmail.com>
2022-12-24 17:21:27 +0200

    Add test_task_creation_form_with_position_assignees_task_type to TaskFormTests in test_forms.py

--- task_manager/models.py
+++ task_manager/models.py
@@ -57,7 +57,7 @@ class Worker(AbstractUser):
     def __str__(self) -> str:
         return (
             f"Username: {self.username} Full name: ({self.first_name}"
-            f" {self.last_name}) position: {self.position.name}"
+            f" {self.last_name})"
         )
 
     def get_absolute_url(self):
--- task_manager/tests/test_forms.py
+++ task_manager/tests/test_forms.py
@@ -2,9 +2,14 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from task_manager.forms import WorkerCreationFrom, PositionForm, TaskTypeForm, CompletedForm, PriorityForm, \
-    WorkerPositionUpdateForm
-from task_manager.models import Position
+from task_manager.forms import (
+    PositionForm,
+    TaskTypeForm,
+    CompletedForm,
+    PriorityForm
+)
+
+from task_manager.models import TaskType, Project
 
 
 class TaskTypeFormTests(TestCase):
@@ -63,3 +68,29 @@ class PriorityFormTests(TestCase):
         form = PriorityForm(data=form_data)
         form.is_valid()
         self.assertEqual(form.cleaned_data, form_data)
+
+
+class TaskFormTests(TestCase):
+
+    def setUp(self) -> None:
+        self.task_type = TaskType.objects.create(name="test")
+        self.project = Project.objects.create(name="test project")
+        self.assignees = get_user_model().objects.create_user(
+            username="user_test",
+            password="test2134",
+            first_name="Test first",
+            last_name="Test last",
+        )
+        self.client.force_login(self.assignees)
+
+    def test_task_creation_form_with_position_assignees_task_type(self):
+        """test should create task form with position & task type &assignees"""
+        task = {
+            "is_completed": "True",
+            "priority": "Ugent",
+            "task_type": self.task_type.id,
+            "assignees": self.assignees.id,
+            "project": self.project.id
+        }
+        response = self.client.post(reverse("task_manager:task-create"), task)
+        self.assertEqual(response.status_code, 200)
